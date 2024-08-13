438d9e0ed7966b5999ed3b44f773d9c3fdd924ed
chijioke <superkingsely@gmail.com>
2024-08-12 18:40:25 -0700

    card show final

--- src/components/checkoutform/CheckoutForm.tsx
+++ src/components/checkoutform/CheckoutForm.tsx
@@ -1,9 +1,33 @@
-import { PaymentElement } from '@stripe/react-stripe-js'
+import { PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
 import React, { useState } from 'react'
 const CheckoutForm = () => {
     const [isprocessing,setIsprocessing]=useState<boolean>(false)
+    const stripe=useStripe()
+    const elements=useElements()
+    const [message,setMessage]=useState<any>(null)
+    // func
     const handlesubmit=async(e:any)=>{
         e.preventDefault()
+        if(!stripe ||!elements){
+            return
+        }
+        setIsprocessing(true)
+        const {error,paymentIntent}:any=await stripe.confirmPayment({
+            elements,
+            confirmParams:{
+                return_url:`${window.location.origin}/success`
+            },
+           
+        })
+        if(error){
+            setMessage(error.message)
+        }else if(paymentIntent&& paymentIntent.status==='succeeded'){
+            setMessage(`payment status ${paymentIntent.status}`)
+        }
+        else{
+            setMessage('hmmm un')
+        }
+        setIsprocessing(false)
     }
   return (
     <form onSubmit={(e)=>handlesubmit(e)}>
@@ -12,8 +36,11 @@ const CheckoutForm = () => {
             <button 
             
             disabled={isprocessing}
-            className={`border p-3 ${isprocessing?'bg-black':'bg-slate-500 cursor-default'}`}>Payment</button>
+            className={`border p-3 ${isprocessing?' bg-slate-500 cursor-default':'bg-black text-white'}`}>Payment</button>
         </div>
+        {
+            message&&<div className='text-red-400'>{message}</div>
+        }
     </form>
   )
 }
